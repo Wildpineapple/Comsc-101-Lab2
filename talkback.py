da58ad4730312405e809c04594ae5f676bba8b80
val <117553455+Wildpineapple@users.noreply.github.com>
2025-05-20 19:44:59 -0400

    Add files via upload

---
+++ talkback.py
@@ -0,0 +1,23 @@
+# I want this program to ask the user for 
+# their name "What's your name?" and then,
+# I want the program to say "Hi, NAME"
+# back where NAME is the user's actaul name
+# I want 
+#step 1, ask the user what their name is
+print("What is your name?")
+# step 2 get the user's name from the keyboard
+# and then save it into a variable so we can use it later
+user_name = input()
+# step 3: say hi back using the user's name 
+print("Hi, " + user_name +".") 
+# asking for their favorite food
+print("What's your fav food?") 
+#variable for storing their fav food
+fav_food =input() 
+# asking how much fav food input they eat a week
+print(fav_food + " is good! How many times you eat it a week?") 
+#variable amount of times eating their fav food
+eat_times = input() 
+#asking user for specefic location to obtain
+print(eat_times + " times is wonderful, any specefic spot for them?")
+rec_spot = input()
